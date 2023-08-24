37461480c108243ce2144555edfa9eb6da13eb9b
Travis Betz <travisaidanbetz@gmail.com>
2023-08-24 19:37:37 -0400

    Second

--- src/ver1/HelloWorld.java
+++ src/ver1/HelloWorld.java
@@ -2,7 +2,7 @@ package ver1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from VSCode!");
+        System.out.println("Hello from Eclipse!");
     }
     
 }
